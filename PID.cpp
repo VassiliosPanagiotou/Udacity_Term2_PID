08983170a11df8ffe3b77cd1ff89924aa51e2a67
VPanagiotou <vassilios.panagiotou@harman.com>
2018-02-14 18:52:59 +0100

    Initial implementation

---
+++ PID.cpp
@@ -0,0 +1,52 @@
+#include "PID.h"
+
+using namespace std;
+
+PID::PID(double Kp, double Ki, double Kd, bool runTwiddle)
+	: m_p_error(0.0)
+	, m_i_error(0.0)
+	, m_d_error(0.0)
+	, m_Kp(Kp)
+	, m_Ki(Ki)
+	, m_Kd(Kd)
+	, m_RunTwiddle(runTwiddle){
+	//
+}
+
+
+double PID::getSteerValue(double cte)
+{
+	m_d_error = cte - m_p_error;
+	m_p_error = cte;
+	m_i_error += cte;
+
+	if (m_RunTwiddle)
+	{
+
+	}
+
+	// return steering value
+	return -(m_Kp * m_p_error + m_Ki * m_i_error + m_Kd * md_error);
+}
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd) {
+
+	m_p_error = 0.0;
+	m_i_error = 0.0;
+	m_d_error = 0.0;
+
+	m_Kp = Kp;
+	m_Ki = Ki;
+	m_Kd = Kd;
+}
+
+void PID::UpdateError(double cte) {
+}
+
+double PID::TotalError() {
+}
+
---
+++ PID.h
@@ -0,0 +1,57 @@
+#ifndef PID_H
+#define PID_H
+
+class PID {
+public:
+	// Ctor
+	PID(double Kp = 0.3, double Ki = 0.0001, double Kd = 1.0, bool runTwiddle = false);
+	// Dtor
+	virtual ~PID();
+
+	///////////////
+	// Given the cross track error, update PID error variables and return steering control value
+	///////////////
+	double getSteerValue(double cte);
+	
+private:
+  /*
+  * Errors
+  */
+  double m_p_error;
+  double m_i_error;
+  double m_d_error;
+
+  /*
+  * Coefficients
+  */ 
+  double m_Kp;
+  double m_Ki;
+  double m_Kd;
+
+  /*
+  * Flag for running Twiddle
+  */
+  bool m_runTwiddle;
+
+  /*
+  * Constructor
+  */
+  PID();
+
+  /*
+  * Initialize PID.
+  */
+  void Init(double Kp, double Ki, double Kd);
+
+  /*
+  * Update the PID error variables given cross track error.
+  */
+  void UpdateError(double cte);
+
+  /*
+  * Calculate the total PID error.
+  */
+  double TotalError();
+};
+
+#endif /* PID_H */
